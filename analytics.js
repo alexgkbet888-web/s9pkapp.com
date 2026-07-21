4d19411477fb7c57a450b449ecc218bed0550785
alexgkbet888-web <alexgkbet888@gmail.com>
2026-07-21 12:49:35 +0900

    Fix S9 GA4 analytics endpoint 2026-07-21

---
+++ analytics.js
@@ -0,0 +1,9 @@
+// S9 portfolio GA4 endpoint for live SEO telemetry checks.
+(function () {
+  var id = "G-Y6WS71WJ22";
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+  window.gtag = window.gtag || gtag;
+  gtag("js", new Date());
+  gtag("config", id, { anonymize_ip: true });
+})();
